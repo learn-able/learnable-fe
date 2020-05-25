841d9d529cabfccc0160360c15b1f2582d845412
Ryan Bahan <ryan@ryanbahan.com>
2020-05-25 14:00:24 -0600

    Complete progress bar

--- src/components/PlaylistViews/PlaylistView4.js
+++ src/components/PlaylistViews/PlaylistView4.js
@@ -1,9 +1,11 @@
 import PlaylistTitle from '../PlaylistTitle/PlaylistTitle'
+import ProgressBar from '../ProgressBar/ProgressBar'
 
 const PlaylistView4 = ({ title, playlistItems }) => {
   return (
     <>
       <PlaylistTitle title={title} playlistItems={playlistItems} />
+      <ProgressBar playlistItems={playlistItems} />
       <div>4</div>
     </>
   )
