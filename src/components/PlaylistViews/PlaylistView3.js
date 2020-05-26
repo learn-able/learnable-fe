aa71826f6aba1e997ed6a329b15c2331b04a5b58
Chris Postma <cjrpostma@gmail.com>
2020-05-25 18:25:14 -0600

    Add NewPlaylistItemBar component.

--- src/components/PlaylistViews/PlaylistView3.js
+++ src/components/PlaylistViews/PlaylistView3.js
@@ -1,15 +1,12 @@
-import PlaylistTitle from '../PlaylistTitle/PlaylistTitle'
-import ProgressBar from '../ProgressBar/ProgressBar'
+import PlaylistTitle from '../PlaylistTitle/PlaylistTitle';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
+const PlaylistView3 = ({ title, playlistItems }) => (
+  <>
+    <PlaylistTitle title={title} playlistItems={playlistItems} />
+    <ProgressBar playlistItems={playlistItems} />
+    <div>3</div>
+  </>
+);
 
-const PlaylistView3 = ({ title, playlistItems }) => {
-  return (
-    <>
-      <PlaylistTitle title={title} playlistItems={playlistItems} />
-      <ProgressBar playlistItems={playlistItems} />
-      <div>3</div>
-    </>
-  )
-}
-
-export default PlaylistView3
+export default PlaylistView3;
